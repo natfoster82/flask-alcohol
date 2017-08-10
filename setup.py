ecca11d22461c4987a345207681caaec1527d776
nat <nat.foster@gmail.com>
2017-08-10 16:58:05 -0600

    version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flask_alcohol',
-      version='0.1',
+      version='0.2',
       description='Automatically generate API routes from Flask-SQLAlchemy models',
       url='https://github.com/natfoster82/flask-alcohol',
       author='Nat Foster',
